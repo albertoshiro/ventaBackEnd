2764556994a0a830695f6ad67756613640b622ea
Alberto Monterrubio <alberto.toshiro@gmail.com>
2021-10-13 17:29:39 -0500

    entendiendo algunos conceptos de los controladores

--- WSVenta/Controllers/WeatherForecastController.cs
+++ WSVenta/Controllers/WeatherForecastController.cs
@@ -11,10 +11,7 @@ namespace WSVenta.Controllers
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
     {
-        private static readonly string[] Summaries = new[]
-        {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        };
+        
 
         private readonly ILogger<WeatherForecastController> _logger;
 
@@ -26,14 +23,15 @@ namespace WSVenta.Controllers
         [HttpGet]
         public IEnumerable<WeatherForecast> Get()
         {
-            var rng = new Random();
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = rng.Next(-20, 55),
-                Summary = Summaries[rng.Next(Summaries.Length)]
-            })
-            .ToArray();
+            List<WeatherForecast> weathers = new List<WeatherForecast>() {
+                new WeatherForecast(){id=1,name="la morza"},
+                new WeatherForecast(){id=20000,name="la morzota"}
+            
+            };
+
+
+            return weathers;
+          
         }
     }
 }
