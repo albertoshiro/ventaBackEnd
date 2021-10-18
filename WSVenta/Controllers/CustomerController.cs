abf26a32f00368b7955307a2a29d8503bbd6ed32
Alberto Monterrubio <alberto.toshiro@gmail.com>
2021-10-18 12:03:21 -0500

    En este commit se realizo el crud al WS

--- WSVenta/Controllers/CustomerController.cs
+++ WSVenta/Controllers/CustomerController.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using WSVenta.Models;
 using WSVenta.Models.Response;
+using WSVenta.Models.Request;
 
 namespace WSVenta.Controllers
 {
@@ -39,6 +40,80 @@ namespace WSVenta.Controllers
                 }
                 return Ok(oResponse);
             } 
+        }
+
+        [HttpPost]
+        public IActionResult Add(CustomerRequest oModel)
+        {
+            var oResponse = new Response();
+            oResponse.Success = 0;
+            try
+            {
+                using (var db = new VENTA_REALContext())
+                {
+                    Customer oCustomer = new Customer();
+                    oCustomer.Name = oModel.Name;
+
+                    db.Customer.Add(oCustomer);
+                    //con esto se guardan los cambios lo de arriba es solo la instruccion
+                    db.SaveChanges();
+                    oResponse.Success = 1;
+                }                
+            }
+            catch ( Exception ex)
+            {
+                oResponse.Message = ex.Message;
+            }
+            return Ok(oResponse);
+        }
+
+        [HttpPut]
+        public IActionResult Edit( CustomerRequest oModel)
+        {
+            var oResponse = new Response();
+            oResponse.Success = 0;
+            try
+            {
+
+                using(var db = new VENTA_REALContext())
+                {
+                    var oCustomer = db.Customer.Find(oModel.Id);
+                    oCustomer.Name = oModel.Name;
+                    db.Entry(oCustomer).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+                    db.SaveChanges();
+                    oResponse.Success = 1;
+                }                
+            }
+            catch (Exception ex)
+            {
+                oResponse.Message = ex.Message;
+            }
+            return Ok(oResponse);
+        }
+
+        //el siguiente metodo http request nos pide un id, este sera pasada por medio de un espacio en la base de la url , aqui se pasara el id para saber que registro deve de eliminar, este se pondra /6 al final de la url por ejemplo indicando que por este medio se le pasara el parametro
+        [HttpDelete("{Id}")]
+        public IActionResult Delete(int id)
+        {
+            var oResponse = new Response();
+            oResponse.Success = 0;
+            try
+            {
+                using (var db = new VENTA_REALContext())
+                {
+                    //buscas un objeto que tenga el mismo id que el que le pasamos
+                    var oCustomer = db.Customer.Find(id);
+                    //remueves un objeto dado antes 
+                    db.Customer.Remove(oCustomer);
+                    db.SaveChanges();
+                    oResponse.Success = 1;
+                }
+            }
+            catch (Exception ex)
+            {
+                oResponse.Message = ex.Message;
+            }
+            return Ok(oResponse);
         }
 
 
---
+++ WSVenta/Models/Request/CustomerRequest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WSVenta.Models.Request
+{
+    public class CustomerRequest
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+    }
+}
